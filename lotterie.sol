630c5bf5c1f70ca2ce14fa824c2d0be83f92570b
Vendrick14 <124353329+Vendrick14@users.noreply.github.com>
2023-05-04 14:27:17 +0200

    Create lotterie.sol

---
+++ lotterie.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Lottery {
+    address payable[] public players;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value == 1 ether, "You need to send 1 ether to enter the lottery");
+        players.push(payable(msg.sender));
+    }
+
+    function random() private view returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players.length)));
+    }
+
+    function pickWinner() public restricted {
+        require(players.length > 0, "There are no players in the lottery");
+        uint256 index = random() % players.length;
+        players[index].transfer(address(this).balance);
+        players = new address payable[](0);
+    }
+
+    modifier restricted() {
+        require(msg.sender == owner, "Only the owner can pick a winner");
+        _;
+    }
